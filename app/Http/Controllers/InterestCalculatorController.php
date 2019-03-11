f7e6e0052f413ad0eab7a665cc3d441742d46b64
Nate Smith <me@nwlsmith.com>
2019-03-11 17:12:23 -0400

    week 6 work

---
+++ app/Http/Controllers/InterestCalculatorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InterestCalculatorController extends Controller
+{
+    public function index()
+    {
+        return 'p3 interestCalculatorController loaded successfully';
+    }
+}
